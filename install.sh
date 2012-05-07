f6a67620a4137342339fa0f0cc78fcaa30f03ed9
James Walker <walkah@walkah.net>
2012-05-07 16:26:45 -0400

    automatically "install" oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
 DOTFILES=`cd $(dirname $0); pwd`
 
 for FILE in $DOTFILES/*; do
