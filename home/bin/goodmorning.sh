3f2436a9fe76dac5f487304b834898a740ef9a77
James Walker <walkah@walkah.net>
2015-01-23 13:44:10 -0500

    clean up good morning

--- home/bin/goodmorning.sh
+++ home/bin/goodmorning.sh
@@ -3,9 +3,7 @@
 # system setup
 sudo chef-client
 rvm gemset reset_env # rvm cookbook breaks this
-brew upgrade 
+brew upgrade
 brew cleanup
-for cask in $(brew cask list); do brew cask install $cask; done
-brew cask cleanup
 # updates from the app store
 softwareupdate -i -a
