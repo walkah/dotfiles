a337f46522e7dc18b9e6863e09a9e2934003bf3b
James Walker <walkah@walkah.net>
2015-01-23 16:18:03 -0500

    rum seems to be working again

--- home/bin/goodmorning.sh
+++ home/bin/goodmorning.sh
@@ -2,7 +2,7 @@
 
 # system setup
 sudo chef-client
-rvm gemset reset_env # rvm cookbook breaks this
+# rvm gemset reset_env # rvm cookbook breaks this
 brew upgrade
 brew cleanup
 # updates from the app store
