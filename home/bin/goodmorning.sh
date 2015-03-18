2224ebe36cd940271c00763fc0fddf6c33a10dd6
James Walker <walkah@walkah.net>
2015-03-17 20:54:10 -0400

    No, I do want this.

--- home/bin/goodmorning.sh
+++ home/bin/goodmorning.sh
@@ -4,6 +4,7 @@
 sudo chef-client
 # rvm gemset reset_env # rvm cookbook breaks this
 brew upgrade
+for i in $(brew cask list); do brew cask install $i; done
 brew cleanup
 # updates from the app store
 softwareupdate -i -a
