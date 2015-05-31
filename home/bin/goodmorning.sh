c91db6b03cc9379a5c6206a687af6df630a2f9f0
James Walker <walkah@walkah.net>
2015-05-31 17:29:56 -0400

    brew upgrade change

--- home/bin/goodmorning.sh
+++ home/bin/goodmorning.sh
@@ -3,7 +3,7 @@
 # system setup
 sudo chef-client
 # rvm gemset reset_env # rvm cookbook breaks this
-brew upgrade
+brew upgrade --all
 for i in $(brew cask list); do brew cask install $i; done
 brew cleanup
 # updates from the app store
