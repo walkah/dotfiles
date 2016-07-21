f18ea91d632c0536e6596350e0f056fd7d8550eb
James Walker <walkah@walkah.net>
2016-07-21 09:58:20 -0400

    shut down docker containers
    (for docker for mac)

--- home/bin/goodnight.sh
+++ home/bin/goodnight.sh
@@ -5,6 +5,7 @@ for box in $(vagrant global-status|grep running|awk '{ print $1 }'); do vagrant 
 
 # shutdown docker machines
 for machine in $(docker-machine ls -q); do docker-machine stop $machine; done
+for container in $(docker ps -a -q); do docker stop $container; done
 
 # kill tmux sessions
 tmux kill-server
