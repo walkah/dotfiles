84e5d4a5c5c3fd79e1156c4cb63fe38658146bc7
James Walker <walkah@walkah.net>
2017-07-30 12:05:21 -0400

    install / use latest ruby, node and python

--- home/bin/goodmorning.sh
+++ home/bin/goodmorning.sh
@@ -3,11 +3,11 @@
 # system setup
 # sudo chef-client
 
-# homesick 
-homesick pull 
+# homesick
+homesick pull
 homesick symlink
 
-# mackup 
+# mackup
 mackup restore
 
 # upgrade homebrew
@@ -16,8 +16,27 @@ brew upgrade
 brew cleanup
 brew cask cleanup
 
-# update gems
+# update ruby + gems
+LATEST_RUBY=$(rbenv install -l | grep -v - | awk '{print $1}'| tail -1)
+if [ $LATEST_RUBY != $(rbenv global) ]; then
+  rbenv install $LATEST_RUBY
+  rbenv global $LATEST_RUBY
+fi
 gem update
+
+# update stable node
+LATEST_NODE=$(nodenv install -l |grep -v - |awk '{print $1}'|egrep '[02468]+\.\d*[0-9]\.[0-9]+'|tail -1)
+if [ $LATEST_NODE != $(nodenv global) ]; then
+  nodenv install $LATEST_NODE
+  nodenv global $LATEST_NODE
+fi
+
+# update python
+LATEST_PYTHON=$(pyenv install -l | grep -v - | awk '{print $1}'|egrep '^2'| tail -1)
+if [ $LATEST_PYTHON != $(pyenv global) ]; then
+  pyenv install $LATEST_PYTHON
+  pyenv global $LATEST_PYTHON
+fi
 
 # update all repositories
 mr -j 5 update
