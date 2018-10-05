1e4fbd084de69f51feb9babfcf8357ca3153d8be
James Walker <walkah@walkah.net>
2018-10-05 00:01:07 -0400

    installer fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing xcode commandline tools..."
-xcode-select --install
+sudo xcode-select --install
 # Mojave fix
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 
@@ -9,11 +9,13 @@ echo "Installing homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s /bin/sh
 
 echo "Installing homesick..."
 sudo gem install homesick
 homesick clone git@github.com:walkah/dotfiles.git
+homesick symlink -f
 
 echo "Initial brew bundle..."
 brew bundle --global
