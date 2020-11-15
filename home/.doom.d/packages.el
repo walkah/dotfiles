8a8e66ae5ac2bf24d5c4794771877b47f1cd584b
James Walker <walkah@walkah.net>
2020-11-15 16:44:51 +0000

    feat(emacs): add pinentry package

--- home/.doom.d/packages.el
+++ home/.doom.d/packages.el
@@ -11,6 +11,7 @@
 
 (package! wakatime-mode)
 (package! prettier-js)
+(package! pinentry)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
