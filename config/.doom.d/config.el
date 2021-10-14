8fd991348db9f2ccab8ed83cd386a068d10f1517
James Walker <walkah@walkah.net>
2021-10-13 21:12:30 -0400

    🧹 not using org-mode

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -30,11 +30,11 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/.org"
-      org-roam-directory "~/.org"
-      deft-directory "~/.org"
-      deft-default-extension "org"
-      deft-recursive t)
+;; (setq org-directory "~/.org"
+      ;; org-roam-directory "~/.org"
+      ;; deft-directory "~/.org"
+      ;; deft-default-extension "org"
+      ;; deft-recursive t)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
