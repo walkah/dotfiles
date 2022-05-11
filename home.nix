213260ff94a799966cab986103a4c92557e53d70
James Walker <walkah@walkah.net>
2022-05-10 22:20:33 -0400

    💄 dracula for tmux

--- home.nix
+++ home.nix
@@ -125,15 +125,26 @@
 
     tmux = {
       enable = true;
+      clock24 = true;
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        yank
+        {
+          plugin = dracula;
+          extraConfig = ''
+            set -g @dracula-show-battery true
+            set -g @dracula-show-fahrenheit false
+            set -g @dracula-show-left-icon session
+            set -g @dracula-show-powerline true
+            set -g @dracula-refresh-rate 10
+          '';
+        }
+      ];
       extraConfig = ''
         set -g set-titles on
         set -g set-titles-string "[#S] #W@#h (#I)"
-
-        set -g status-bg black
-        set -g status-fg white
       '';
       shortcut = "o";
-      terminal = "screen-256color";
     };
 
     emacs = {
