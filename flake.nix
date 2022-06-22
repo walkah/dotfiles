e8d4263e65cd95281d88b87f6935ec2a36154668
James Walker <walkah@walkah.net>
2022-06-22 16:32:51 -0400

    🔧 clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -3,49 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
-    homeConfigurations = {
-      aarch64-darwin = home-manager.lib.homeManagerConfiguration {
-        system = "aarch64-darwin";
-        homeDirectory = "/Users/walkah";
-        username = "walkah";
-        stateVersion = "21.03";
-        configuration = { config, pkgs, ... }: {
-          imports = [
-            ./home.nix
-          ];
-        };
-      };
-      x86_64-darwin = home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-darwin";
-        homeDirectory = "/Users/walkah";
-        username = "walkah";
-        stateVersion = "21.03";
-        configuration = { config, pkgs, ... }: {
-          imports = [
-            ./home.nix
-          ];
-        };
-      };
-      x86_64-linux = home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        homeDirectory = "/home/walkah";
-        username = "walkah";
-        stateVersion = "21.03";
-        configuration = { config, pkgs, ... }: {
-          imports = [
-            ./home.nix
-          ];
-        };
-      };
-
-    };
-    defaultPackage.aarch64-darwin = self.homeConfigurations.aarch64-darwin.activationPackage;
-    defaultPackage.x86_64-darwin = self.homeConfigurations.x86_64-darwin.activationPackage;
-    defaultPackage.x86_64-linux = self.homeConfigurations.x86_64-linux.activationPackage;
-  };
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          homeConfigurations = {
+            walkah = home-manager.lib.homeManagerConfiguration {
+              system = system;
+              homeDirectory = if pkgs.stdenv.isDarwin then "/Users/walkah" else "/home/walkah";
+              username = "walkah";
+              stateVersion = "21.03";
+              configuration = { config, pkgs, ... }: {
+                imports = [ ./home.nix ];
+              };
+            };
+          };
+          packages.default =
+            self.homeConfigurations.${system}.walkah.activationPackage;
+        });
 }
