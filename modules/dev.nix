94b2a83bc89509cca84c314fd7c1c9c63a7f56e1
James Walker <walkah@walkah.net>
2022-09-02 19:23:29 -0400

    🔥 remove dev languages / tools (again)
    dear future me: you don't want all this stuff on all machines where your
    dotfiles are.

--- modules/dev.nix
+++ modules/dev.nix
@@ -6,33 +6,12 @@
     btop
     cmake
     drone-cli
-    tea
 
     # Git
     git
     gh
     mr
-
-    # Elixir
-    elixir
-
-    # Golang
-    go
-    gomodifytags
-    gotests
-    gore
-
-    # Haskell
-    haskellPackages.haskell-language-server
-    # haskellPackages.cabal
-    haskellPackages.hoogle
-    stack
-
-    # Javascript/Typescript
-    # deno
-    jq
-    nodejs
-    yarn
+    tea
 
     # Nix
     cachix
@@ -40,27 +19,6 @@
     nixfmt
     nixpkgs-fmt
     rnix-lsp
-
-    # Python
-    black
-    isort
-    pipenv
-    python39
-    python39Packages.pyflakes
-    python39Packages.nose
-    python39Packages.pytest
-
-    # Rust
-    rustup
-    rust-analyzer
-
-    # Shell
-    shfmt
-    shellcheck
-
-    # Web
-    nodePackages.stylelint
-    nodePackages.js-beautify
   ];
 
   home.file.".ghci".text = ''
