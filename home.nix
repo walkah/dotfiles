65e0539591df9939c2c1aba34d70d6d7d52c79ea
James Walker <walkah@walkah.net>
2023-02-20 13:38:28 -0500

    📌 bump home.stateVersion

--- home.nix
+++ home.nix
@@ -23,5 +23,5 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.05";
+  home.stateVersion = "23.05";
 }
